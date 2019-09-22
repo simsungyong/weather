922844cf87a8c4f06546f1cd524afe614d97e040
Sim sungyong <ssy332972@gmail.com>
2019-09-22 20:49:02 +0900

    commit 3

--- App.js
+++ App.js
@@ -3,18 +3,29 @@ import { StyleSheet, Text, View } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 import {Alert} from 'react-native';
+import axios from 'axios';
 
+const API_KEY = "c743c0fba00b6f2bf6c367ef61dbd746"
 export default class extends React.Component {
   state = {
     isLoading : true
   }
+  getWeather = async(latitude,longitude)=>{
+    const{data} = await axios.get(
+      `https://samples.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+      );
+      console.log(data);
+  }
+
   getLocation = async() =>{
     try {
       //throw Error();
+    
       await Location.requestPermissionsAsync();
-      const {coords} = await Location.getCurrentPositionAsync();
-      //this.setState({isLoading:false})
-      console.log(coords.latitude, coords.longitude);      
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({isLoading:false})
+      
     } catch (error) {
       Alert.alert("Can't find you");
     }

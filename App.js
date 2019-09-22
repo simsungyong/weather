a89ef96bca30b9bca3aa3f447a8748f738c5fa29
Sim sungyong <ssy332972@gmail.com>
2019-09-22 19:55:46 +0900

    commit 2

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Loading from './Loading';
+import * as Location from 'expo-location';
+import {Alert} from 'react-native';
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  }
+  getLocation = async() =>{
+    try {
+      //throw Error();
+      await Location.requestPermissionsAsync();
+      const {coords} = await Location.getCurrentPositionAsync();
+      //this.setState({isLoading:false})
+      console.log(coords.latitude, coords.longitude);      
+    } catch (error) {
+      Alert.alert("Can't find you");
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading} = this.state;
+    return isLoading ? <Loading />: null;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- Loading.js
+++ Loading.js
@@ -9,16 +9,16 @@ export default function Loading(){
     );
 }
 
-const sytles=StyleSheet.create({
+const styles=StyleSheet.create({
     container:{
         flex:1,
         justifyContent: "flex-end",
-        paddingHorizontal:20,
+        paddingHorizontal:30,
         paddingVertical:100,
         backgroundColor: '#FDF6AA'
     },
     text: {
-        color: '#2c2c2c',
+        color: "#2c2c2c",
         fontSize: 30
     }
 });

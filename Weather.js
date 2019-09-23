6a0f2aa4920c32d53f56656a7eed1e27f537e1be
Sim sungyong <ssy332972@gmail.com>
2019-09-23 21:31:47 +0900

    commit

--- Weather.js
+++ Weather.js
@@ -9,39 +9,57 @@ import {LinearGradient} from 'expo-linear-gradient';
 const weatherOptions = {
     Clear : {
         iconName: 'weather-sunny',
-        gradient:['#F7FE2E','#FE9A2E']
+        gradient:['#F7FE2E','#FE9A2E'],
+        title: "Sunny",
+        subtitle: "fucking hot"
     },
     Thunderstorm:{
         iconName:"weather-hurricane",
-        gradient:['#210B61','#220A29']
+        gradient:['#210B61','#220A29'],
+        title: "Thunderstorm",
+        subtitle: "wow... Don't go outside"
     },
     Drizzle:{
         iconName:"weather-partlycloudy",
-        gradient:['#A4A4A4','#1C1C1C']
+        gradient:['#A4A4A4','#1C1C1C'],
+        title:"Drizzle",
+        subtitle:"Wow... drizzle fufk"
     },
     Rain:{
         iconName:"weather-rainy",
-        gradient:['#086A87','#0A2229']
+        gradient:['#086A87','#0A2229'],
+        title:"Rain",
+        subtitle: "막걸리엔 파전~"
     },
     Snow:{
         iconName:"weather-snowy",
-        gradient:['#E6E6E6','#0B2161']
+        gradient:['#E6E6E6','#0B2161'],
+        title:"Snow",
+        subtitle:"첫 눈.."
     },
     Clouds:{
         iconName:"weather-cloudy",
-        gradient:['#A4A4A4','#1C1C1C']
+        gradient:['#A4A4A4','#1C1C1C'],
+        title:"Clouds",
+        subtitle:"우울..."
     },
     Haze:{
         iconName:"weather-hail",
-        gradient:['#A4A4A4','#1C1C1C']
+        gradient:['#A4A4A4','#1C1C1C'],
+        title:"Haze",
+        subtitle:"sfsf"
     },
     Fog:{
         iconName:"weather-sunset-down",
-        gradient:['#A4A4A4','#1C1C1C']
+        gradient:['#A4A4A4','#1C1C1C'],
+        title:"Fog",
+        subtitle:"fsfsf"
     },
     Mist:{
         iconName:"weather-sunset-up",
-        gradient:['#A4A4A4','#1C1C1C']
+        gradient:['#A4A4A4','#1C1C1C'],
+        title:"Mist",
+        subtitle:"sfsfsfsf"
     }
 };
 
@@ -49,20 +67,20 @@ export default function Weather({temp, condition}){
     return (
     
         <LinearGradient
-          colors={weatherOptions['Clear'].gradient}
+          colors={weatherOptions[condition].gradient}
           style={styles.container}>
             <StatusBar barStyle="light-content" />
             <View style={styles.halfcontainer}>
                 <MaterialCommunityIcons
                     size ={86} 
-                    name={weatherOptions['Clear'].iconName} 
+                    name={weatherOptions[condition].iconName} 
                     color='white'/>
                 <Text style={styles.temp}>{temp}</Text>
             </View>
-                <View style={styles.halfcontainer}>
+                <View style={{...styles.halfcontainer, ...styles.textContainer}}>
                     <View>
-                        <Text style={styles.title}>Title</Text>
-                        <Text style={styles.subtitle}>Subtitle</Text>
+                        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                        <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
                     </View>
 
             </View>
@@ -114,6 +132,9 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 24,
         fontWeight:'200'
+    },
+    textContainer:{
+        paddingHorizontal:20
     }
 
 });
